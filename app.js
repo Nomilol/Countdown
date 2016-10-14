30ee59596549d9513d61593271200ea8ab5d575c
Axel <lezinaxel@gmail.com>
2016-10-14 13:58:40 +0200

    Fonction reset ajoutée

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 var Interval;
-var time = 65;
+var time;
 
 function compteur(){
-	time--;
-	var min = parseInt(time/60,10);
-	var sec = time - min*60;
-	if(time === 0 && min != 0){
-		min--;
-	} if(sec === 0 && min === 0){
-		clearInterval(Interval);
-	}
+	Interval = setInterval(function(){
+		time--;
+		var min = parseInt(time/60,10);
+		var sec = time - min*60;
+		if(time === 0 && min != 0){
+			min--;
+		} if(sec === 0 && min === 0){
+			clearInterval(Interval);
+		}
+		$("#minutes").text(min);
+		$("#secondes").text(sec);
+	}, 1000);
+	$("#playstop").on("click", function(){
+		clearInterval(Interval);		
+		
+	}); 
+}
 
-$("#minutes").text(min);
-$("#secondes").text(sec);
-};
+
+function reset(){
+	compteur();
+	time = 65;
+}
+
+$("#reset").click(function(){
+	reset();
+});
 
 
 
-Interval = setInterval(compteur, 1000);
-
-
-$("#playstop").on("click", function(){
-	clearInterval(Interval);		
-	
-}); 
 
